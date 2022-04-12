593ca4f8d02c6cd13f015387ff472e9849fb47d7
y_h <y_h@kingdee.com>
2022-04-12 17:23:29 +0800

    1. 增加事件监听事件的实现类

---
+++ e-commerce-gateway/src/main/java/cn/kissy/ecommerce/config/GatewayConfig.java
@@ -0,0 +1,60 @@
+package cn.kissy.ecommerce.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类，读取Nacos相关的配置项，用于配置监听器
+ * @ClassName GatewayConfig
+ * @Author kingdee
+ * @Date 2022/4/11
+ * @Version 1.0
+ **/
+@Configuration
+public class GatewayConfig {
+
+    /**
+     * 读取配置的超时时间
+     */
+    public static final long DEFAULT_TIMEOUT = 30000;
+
+    /**
+     * Nacos 服务器地址
+     */
+    public static String NACOS_SERVER_ADDR;
+
+    /**
+     * 命名空间
+     */
+    public static String NACOS_NAMESPACE;
+
+    /**
+     * data-id
+     */
+    public static String NACOS_ROUTE_DATA_ID;
+
+    /**
+     * 分组 id
+     */
+    public static String NACOS_ROUTE_GROUP;
+
+    @Value("${spring.cloud.nacos.discovery.server-addr}")
+    public void  setNacosServerAddr(String nacosServerAddr){
+        NACOS_SERVER_ADDR = nacosServerAddr;
+    }
+
+    @Value("${spring.cloud.nacos.discovery.namespace}")
+    public void setNacosNamespace(String nacosNamespace) {
+        NACOS_NAMESPACE = nacosNamespace;
+    }
+
+    @Value("${nacos.gateway.route.config.data-id}")
+    public void setNacosRouteDataId(String nacosRouteDataId) {
+        NACOS_ROUTE_DATA_ID = nacosRouteDataId;
+    }
+
+    @Value("${nacos.gateway.route.config.group}")
+    public static void setNacosRouteGroup(String nacosRouteGroup) {
+        NACOS_ROUTE_GROUP = nacosRouteGroup;
+    }
+}
